b43f8cf48d89e624ad64b4a4dcb001a116cc6ca7
fhenrion <fhenrion@e2r7p10.42.fr>
2020-01-03 17:30:58 +0100

    disassemble pt1

--- CPU.c
+++ CPU.c
@@ -6,39 +6,66 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/01 17:39:10 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/01/01 18:36:34 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/01/03 17:29:20 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CPU.h"
 
-void	emulate_8080(t_state *state)
+// essayer plusieurs méthodes d'écriture
+static int		disassemble_8080(uint8_t *memory, uint16_t pc)
 {
-	int		cycles = 4; // pourquoi ?
-	uint8_t	*opcode = state->memory;
+	uint8_t	*code = &memory[pc];
+	int		opbytes = 1;
 
-	// state->pc++; ? while quoi ? trouver comment s'arrêter
-	// TODO : encore tout^^
-	// deux méthodes :
-	// un énorme switch
-	// OU
-	// avoir un tableau de pointeurs sur fonction
-	// parcour d'un tableau des codes renvoyant un index
-	// execution de la fonction a cet index dans le tableau de pointeur
+	printf ("%04x ", pc);
+	switch (*code)
+	{
+		case 0x00: printf("NOP"); break;
+		case 0x01: printf("LXI	B,#$%02x%02x", code[2], code[1]);
+		opbytes=3; break;
+		case 0x02: printf("STAX	B"); break;
+		case 0x03: printf("INX	B"); break;
+		case 0x04: printf("INR	B"); break;
+		case 0x05: printf("DCR	B"); break;
+		case 0x06: printf("MVI	B,#$%02x", code[1]);
+		opbytes=2; break;
+		case 0x07: printf("RLC"); break;
+		case 0x08: printf("NOP"); break;
+		/* ........ */
+		case 0x3e: printf("MVI	A,#0x%02x", code[1]);
+		opbytes = 2; break;
+		/* ........ */
+		case 0xc3: printf("JMP	$%02x%02x",code[2],code[1]);
+		opbytes = 3;break;
+		/* ........ */
+	}
+
+	printf("\n");
+	return (opbytes); // add to pc ??
 }
 
-t_error	load_into_memory(t_state *state, char *filename)
+static void		emulate_8080(t_state *state)
+{
+	//
+}
+
+static t_error	load_into_memory(t_state *state, char *filename)
 {
 	int		fd;
+	int		ROM_size;
+	int		pc;
 
 	if ((fd = open(filename, O_RDONLY)) == -1)
 		return (ERROR);
-	read(fd, state->memory, 0x10000);
+	ROM_size = read(fd, state->memory, 0x10000);
 	close(fd);
+	while (pc < ROM_size)
+		pc += disassemble_8080(state->memory, state->pc);
 	return (NO_ERROR);
 }
 
-int		main(int ac, char **av)
+int				main(int ac, char **av)
 {
 	t_state	state;
 
@@ -53,7 +80,7 @@ int		main(int ac, char **av)
 		return (0);
 	}
 	// initialiser les flags ?
-	state.pc = 0;
+	state.pc = 0; // dans quels cas pas 0 ?
 	emulate_8080(&state);
 	return (0);
 }
