6ac47a63cadded5395f9c414552b1c8fab62646f
Flavien Henrion <flavien.henrion@protonmail.com>
2020-01-09 16:36:16 +0100

    update

--- disassemble_instructions.c
+++ disassemble_instructions.c
@@ -6,7 +6,7 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 12:08:59 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/01/09 14:14:55 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/01/09 16:36:08 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,250 +14,222 @@
 
 t_opsize	NOP(t_memory *mem)
 {
-	(void*)mem;
-	printf("NOP\n");
+	printf("$%02x :NOP\n", *mem);
 	return (1);
 }
 
 t_opsize	RLC(t_memory *mem)
 {
-	(void*)mem;
-	printf("RLC\n");
+	printf("$%02x :RLC\n", *mem);
 	return (1);
 }
 
 t_opsize	LXI_B(t_memory *mem)
 {
-	printf("LXI    B,#$%02x%02x\n", mem[2], mem[1]);
+	printf("$%02x :LXI    B,#$%02x%02x\n", *mem, mem[2], mem[1]);
 	return (3);
 }
 
 t_opsize	LXI_D(t_memory *mem)
 {
-	printf("LXI    D,#$%02x%02x\n", mem[2], mem[1]);
+	printf("$%02x :LXI    D,#$%02x%02x\n", *mem, mem[2], mem[1]);
 	return (3);
 }
 
 t_opsize	LXI_H(t_memory *mem)
 {
-	printf("LXI    H,#$%02x%02x\n", mem[2], mem[1]);
+	printf("$%02x :LXI    H,#$%02x%02x\n", *mem, mem[2], mem[1]);
 	return (3);
 }
 
 t_opsize	STAX_B(t_memory *mem)
 {
-	(void*)mem;
-	printf("STAX   B\n");
+	printf("$%02x :STAX   B\n", *mem);
 	return (1);
 }
 
 t_opsize	STAX_D(t_memory *mem)
 {
-	(void*)mem;
-	printf("STAX   D\n");
+	printf("$%02x :STAX   D\n", *mem);
 	return (1);
 }
 
 t_opsize	INX_B(t_memory *mem)
 {
-	(void*)mem;
-	printf("INX    B\n");
+	printf("$%02x :INX    B\n", *mem);
 	return (1);
 }
 
 t_opsize	INX_D(t_memory *mem)
 {
-	(void*)mem;
-	printf("INX    D\n");
+	printf("$%02x :INX    D\n", *mem);
 	return (1);
 }
 
 t_opsize	INX_H(t_memory *mem)
 {
-	(void*)mem;
-	printf("INX    H\n");
+	printf("$%02x :INX    H\n", *mem);
 	return (1);
 }
 
 t_opsize	INR_B(t_memory *mem)
 {
-	(void*)mem;
-	printf("INR    B\n");
+	printf("$%02x :INR    B\n", *mem);
 	return (1);
 }
 
 t_opsize	INR_C(t_memory *mem)
 {
-	(void*)mem;
-	printf("INR    C\n");
+	printf("$%02x :INR    C\n", *mem);
 	return (1);
 }
 
 t_opsize	INR_D(t_memory *mem)
 {
-	(void*)mem;
-	printf("INR    D\n");
+	printf("$%02x :INR    D\n", *mem);
 	return (1);
 }
 
 t_opsize	INR_E(t_memory *mem)
 {
-
-	(void*)mem;
-	printf("INR    E\n");
+	printf("$%02x :INR    E\n", *mem);
 	return (1);
 }
 
 t_opsize	INR_H(t_memory *mem)
 {
-
-	(void*)mem;
-	printf("INR    H\n");
+	printf("$%02x :INR    H\n", *mem);
 	return (1);
 }
 
 t_opsize	DCR_B(t_memory *mem)
 {
-	(void*)mem;
-	printf("DCR    B\n");
+	printf("$%02x :DCR    B\n", *mem);
 	return (1);
 }
 
 t_opsize	DCR_C(t_memory *mem)
 {
-	(void*)mem;
-	printf("DCR    C\n");
+	printf("$%02x :DCR    C\n", *mem);
 	return (1);
 }
 
 t_opsize	DCR_D(t_memory *mem)
 {
-	(void*)mem;
-	printf("DCR    D\n");
+	printf("$%02x :DCR    D\n", *mem);
 	return (1);
 }
 
 t_opsize	DCR_E(t_memory *mem)
 {
-	(void*)mem;
-	printf("DCR    E\n");
+	printf("$%02x :DCR    E\n", *mem);
 	return (1);
 }
 
 t_opsize	DCR_H(t_memory *mem)
 {
-	(void*)mem;
-	printf("DCR    H\n");
+	printf("$%02x :DCR    H\n", *mem);
 	return (1);
 }
 
 t_opsize	MVI_A(t_memory *mem)
 {
-	printf("MVI    A,#$%02x\n", mem[1]);
+	printf("$%02x :MVI    A,#$%02x\n", *mem, mem[1]);
 	return (2);
 }
 
 t_opsize	MVI_B(t_memory *mem)
 {
-	printf("MVI    B,#$%02x\n", mem[1]);
+	printf("$%02x :MVI    B,#$%02x\n", *mem, mem[1]);
 	return (2);
 }
 
 t_opsize	MVI_C(t_memory *mem)
 {
-	printf("MVI    C,#$%02x\n", mem[1]);
+	printf("$%02x :MVI    C,#$%02x\n", *mem, mem[1]);
 	return (2);
 }
 
 t_opsize	MVI_D(t_memory *mem)
 {
-	printf("MVI    D,#$%02x\n", mem[1]);
+	printf("$%02x :MVI    D,#$%02x\n", *mem, mem[1]);
 	return (2);
 }
 
 t_opsize	MVI_E(t_memory *mem)
 {
-	printf("MVI    E,#$%02x\n", mem[1]);
+	printf("$%02x :MVI    E,#$%02x\n", *mem, mem[1]);
 	return (2);
 }
 
 t_opsize	MVI_H(t_memory *mem)
 {
-	printf("MVI    H,#$%02x\n", mem[1]);
+	printf("$%02x :MVI    H,#$%02x\n", *mem, mem[1]);
 	return (2);
 }
 
 t_opsize	DAD_B(t_memory *mem)
 {
-	(void*)mem;
-	printf("DAD    B\n");
+	printf("$%02x :DAD    B\n", *mem);
 	return (1);
 }
 
 t_opsize	DAD_D(t_memory *mem)
 {
-	(void*)mem;
-	printf("DAD    D\n");
+	printf("$%02x :DAD    D\n", *mem);
 	return (1);
 }
 
 t_opsize	LDAX_B(t_memory *mem)
 {
-	(void*)mem;
-	printf("LDAX   B\n");
+	printf("$%02x :LDAX   B\n", *mem);
 	return (1);
 }
 
 t_opsize	LDAX_D(t_memory *mem)
 {
-	(void*)mem;
-	printf("LDAX   D\n");
+	printf("$%02x :LDAX   D\n", *mem);
 	return (1);
 }
 
 t_opsize	DCX_B(t_memory *mem)
 {
-	(void*)mem;
-	printf("DCX    B\n");
+	printf("$%02x :DCX    B\n", *mem);
 	return (1);
 }
 
 t_opsize	DCX_D(t_memory *mem)
 {
-	(void*)mem;
-	printf("DCX    D\n");
+	printf("$%02x :DCX    D\n", *mem);
 	return (1);
 }
 
 t_opsize	RRC(t_memory *mem)
 {
-	(void*)mem;
-	printf("RRC\n");
+	printf("$%02x :RRC\n", *mem);
 	return (1);
 }
 
 t_opsize	RAL(t_memory *mem)
 {
-	(void*)mem;
-	printf("RAL\n");
+	printf("$%02x :RAL\n", *mem);
 	return (1);
 }
 
 t_opsize	RAR(t_memory *mem)
 {
-	(void*)mem;
-	printf("RAR\n");
+	printf("$%02x :RAR\n", *mem);
 	return (1);
 }
 
 t_opsize	SHLD(t_memory *mem)
 {
-	printf("SHLD    $%02x%02x\n", mem[2], mem[1]);
+	printf("$%02x :SHLD    $%02x%02x\n", *mem, mem[2], mem[1]);
 	return (3);
 }
 
 t_opsize	JMP(t_memory *mem)
 {
-	printf("JMP     $%02x%02x\n", mem[2], mem[1]);
+	printf("$%02x :JMP     $%02x%02x\n", *mem, mem[2], mem[1]);
 	return (3);
 }
