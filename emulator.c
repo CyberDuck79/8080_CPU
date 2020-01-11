696f6eeb094aad3797b1068c111ccc6421101b06
Flavien Henrion <flavien.henrion@protonmail.com>
2020-01-11 13:28:51 +0100

    mise en place emulation

--- emulator.c
+++ emulator.c
@@ -6,7 +6,7 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/01 17:39:10 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/01/11 12:52:30 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/01/11 13:11:54 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,14 @@ static t_opsize	disassemble_8080(t_computer *computer)
 }
 
 /*
+// return error if trying to write on ROM addr space ?
 static void		emulate_8080(t_cpu *cpu)
 {
 	
 }
 */
 
-static t_error	load_into_memory(t_computer *computer, char **ROM_files)
+static t_error	load_into_memory(t_memory mem[0x2000], char **ROM_files)
 {
 	uint8_t		index = 0;
 	uint16_t	offset[5] = {0, 0x800, 0x1000, 0x1800, 0x2000};
@@ -41,11 +42,11 @@ static t_error	load_into_memory(t_computer *computer, char **ROM_files)
 		if (fd == -1)
 			return (ERROR);
 		addr_space = offset[index + 1] - offset[index];
-		read(fd, computer->mem + offset[index], addr_space);
+		read(fd, mem + offset[index], addr_space);
 		close(fd);
 		index++;
 	}
-	computer->ROM_size = 0x2000;
+	
 	return (NO_ERROR);
 }
 
@@ -54,12 +55,13 @@ int				main(int ac, char **av)
 	t_computer	computer;
 
 	bzero(&computer, sizeof(computer));
+	computer.ROM_size = 0x2000;
 	if (ac != 5)
 	{
 		write(1, "USAGE : ROM filenames .h .g .f .e\n", 34);
 		return (0);
 	}
-	if (load_into_memory(&computer, av + 1))
+	if (load_into_memory(computer.mem, av + 1))
 	{
 		write(1, "error at opening files.\n", 23);
 		return (0);
@@ -67,7 +69,7 @@ int				main(int ac, char **av)
 	disassembler_instructions_ini(computer.asm_instructions_bus);
 	while (computer.cpu.reg.PC < computer.ROM_size)
 		computer.cpu.reg.PC += disassemble_8080(&computer);
-	computer.cpu.reg.PC = 0;
+	//computer.cpu.reg.PC = 0;
 	//emulation_instructions_ini(computer.cpu.emu_instructions_bus);
 	//emulate_8080(&cpu);
 	return (0);
---
+++ emulator_ini.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   emulator_ini.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/11 13:25:27 by fhenrion          #+#    #+#             */
+/*   Updated: 2020/01/11 13:25:56 by fhenrion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EMULATOR_INI_H
+# define EMULATOR_INI_H
+
+#include "emulator.h"
+
+void	emulator_instructions_ini(t_emu_inst intructions_bus[0x100]);
+
+#endif
